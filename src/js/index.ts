b713ffaba5573287f23dfc69b969d354816e6567
Kristján Oddsson <koddsson@gmail.com>
2023-07-03 14:12:55 +0200

    init

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,14 @@
-// Put the code here
+const params = new URLSearchParams(window.location.search)
+const texts = params.getAll('text')
+
+// eslint-disable-next-line i18n-text/no-en
+let text = 'Inspirational Quote'
+
+if (texts.length > 0) {
+  text = texts[Math.floor(Math.random() * texts.length)]
+}
+
+const h1 = document.querySelector<HTMLElement>('h1')!
+h1.textContent = `"${text}"`
+
+export {}
