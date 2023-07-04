c9876835c75f5b99fa00327c5c17e95723c94348
Kristján Oddsson <koddsson@gmail.com>
2023-07-04 22:48:47 +0200

    actually use Rollup and PostCSS together

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,27 @@
 import {rollupPluginHTML as html} from '@web/rollup-plugin-html'
 import esbuild from 'rollup-plugin-esbuild'
 import {nodeResolve} from '@rollup/plugin-node-resolve'
+import postcss from 'postcss';
+
+import postCssConfig from './postcss.config.js'
 
 export default {
   input: 'index.html',
   output: {dir: 'dist'},
-  plugins: [nodeResolve(), esbuild(), html({input: 'index.html'})],
+  plugins: [
+    nodeResolve(),
+    esbuild(),
+    html({
+      input: 'index.html',
+      transformAsset: [
+        async (content, filePath) => {
+          if (filePath.endsWith('css')) {
+            return (
+              await postcss(postCssConfig.plugins).process(content.toString('utf-8'), {from: undefined, to: undefined})
+            ).css
+          }
+        },
+      ],
+    }),
+  ],
 }
