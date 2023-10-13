ab357bc6718fcc134e9dd734e0e4d6ddcf0b840e
Kristján Oddsson <koddsson@gmail.com>
2023-10-13 21:27:51 +0200

    Add a actual test

--- tests/index.ts
+++ tests/index.ts
@@ -1,7 +1,9 @@
 import {assert} from '@open-wc/testing'
+import '../src/js/index.js'
 
 describe('my tests', () => {
-  it('adds two numbers', () => {
-    assert.equal(2 + 2, 4)
+  it("Set's the heading of a website to 'Inspirational Quote' in initial load", () => {
+    const h1 = document.querySelector<HTMLHeadingElement>('h1')
+    assert.equal(h1?.textContent, '"Inspirational Quote"')
   })
 })
